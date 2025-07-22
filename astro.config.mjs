a41f89247357efc7e84b5022136e13594037acc1
andyzhou <andyforever0108@outlook.com>
2025-07-22 14:57:23 +0800

    Deploy to Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://andyzhouty.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
